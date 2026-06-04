620b9ed9ae59fa6c9961be146b410647a53340bb
OguzhanUmutlu <64753457+OguzhanUmutlu@users.noreply.github.com>
2026-06-04 20:18:55 +0300

    v0.9.9 fixes

--- src/reignite/elements/plugins/gui/WorldStats.py
+++ src/reignite/elements/plugins/gui/WorldStats.py
@@ -15,7 +15,7 @@ class WorldStatsPlugin(Plugin):
 
                  name="World stats", **gui_kwargs
                  ):
-        super().__init__(name=name, filename="WorldControl", elements=[
+        super().__init__(name=name, filename="WorldStats", elements=[
             GzGui(
                 **{"title": "World stats", "show_title_bar": True, "resizable": False, "height": 110.0, "width": 290.0,
                    "z": 1.0, "state": "floating", "anchor": "3D View", "anchors": [
