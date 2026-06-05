56c4718d75064a10e0c0d61b9e6f72bd7ff47345
OguzhanUmutlu <64753457+OguzhanUmutlu@users.noreply.github.com>
2026-06-05 04:33:03 +0300

    v0.11.4 added frames to pose

--- src/reignite/utils/__init__.py
+++ src/reignite/utils/__init__.py
@@ -2,7 +2,7 @@ from .color import Color
 from .errors import SDFError
 from .migration import cmp_version, apply_migrations
 from .model import BaseModel
-from .pose import Pose
+from .pose import Pose, PoseAP
 from .vector2d import Vector2d
 from .vector3 import Vector3
 from .utils import *
